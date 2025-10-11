071201a85b7f3137276c1e49706e759bdb528531
sehnxxxx <13736471390@163.com>
2025-10-11 14:17:55 +0800

    4:51:14点赞likes做完

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -197,3 +197,37 @@ export const updateProfile = async (formData:FormData,cover:string)=>{
         return ;
     }
 };
+
+export const switchLike = async (postId: number) => {
+    const { userId } = auth();
+
+    if (!userId) throw new Error("User is not authenticated!");
+
+    try {
+        const existingLike = await prisma.like.findFirst({
+            where: {
+                postId,
+                userId,
+            },
+        });
+
+        if (existingLike) {
+            await prisma.like.delete({
+                where: {
+                    id: existingLike.id,
+                },
+            });
+        } else {
+            await prisma.like.create({
+                data: {
+                    postId,
+                    userId,
+                },
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        throw new Error("Something went wrong");
+    }
+};
+
